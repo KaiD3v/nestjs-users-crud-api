91c9611f0ba6840cc52d6c15feedc9a821fc7509
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-06-15 14:10:05 -0300

    prisma config

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma.service";
+import { PrismaClient } from "@prisma/client";
+
+@Module({
+    providers: [PrismaService], // declara que prisma service faz parte deste modulo
+    exports: [PrismaModule] // exporta pra que seja usado em outros lugares
+})
+
+export class PrismaModule {
+
+
+
+}
