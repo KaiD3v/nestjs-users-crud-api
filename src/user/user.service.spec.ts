37082cd68b90b3c70cf2e41db471d24715b0fd6b
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-06-24 14:07:45 -0300

    create user test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,6 +2,8 @@ import { TestingModule, Test } from "@nestjs/testing"
 import { UserService } from "./user.service"
 import { PrismaService } from "../prisma/prisma.service";
 import { userPrismaMock } from "../testing/user-prisma.mock";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { Rule } from "../enums/rule.enum";
 
 describe('UserService', () => {
 
@@ -22,8 +24,26 @@ describe('UserService', () => {
         expect(userService).toBeDefined()
     })
 
-    describe('Create', async () => {})
-    describe('Create', async () => {})
-    describe('Create', async () => {})
+    describe('Create Method', () => {
+        test('should create a new user', async () => {
+            const data: CreateUserDTO = {
+                name: 'Create Test',
+                email: 'createtest@gmail.com',
+                password: '123456',
+                rule: Rule.User
+            };
+
+            const expectedResult = {...data};
+            (userService.create as jest.Mock).mockResolvedValue(expectedResult);
+
+            const result = await userService.create(data);
+
+            expect(userService.create).toHaveBeenCalledWith(data);
+            expect(result).toBe(expectedResult);
+        });
+    })
+    describe('Read', () => {})
+    describe('Update', () => {})
+    describe('Delete', () => {})
 
 })
